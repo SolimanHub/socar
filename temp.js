48567677f8f153d41e7d49046ec039e13777ceaa
solimanhub <david.soliman2010@gmail.com>
2020-07-08 14:27:33 -0600

    no recuerdo las mod pero funciona

--- temp.js
+++ temp.js
@@ -1,140 +1,18 @@
-/*
-poner payout en 2
-*/
+clear();
+var apuesta_inicial = 0.00000001;
 
-console.time('tiempo_en_ejecucion');
+setAmount(3, true);
+Dice_CalculateAmounts(1, true); // actualiza el profit
 
-var derrotas_permitidas=30;
-var limite=1; //cantidad de victorias maxima
-var apuesta_inicial = 0.0000001;
+for (var i = 1; i <= 40; i++) {
+    var bet_amt = $("input[name='base_bet']").val();
+    var profit = $("input[name='profit_auto']").val();
 
-var victorias = 0; //contador de victorias
-var mayor_derrota=0; //registra el numero mayor de derrotas antes de una victoria
-var jhg=1; // jugadas antes de ganar.
-var jhg_arr = [];
-var jhg_arr_rep = [];
-var tamanio=0;
-clear(); 
+    console.log(bet_amt+"   "+profit);
 
-Roll_HiLo_Dice_a_mod();
-
-function Roll_HiLo_Dice_a_mod() {
-    monto(3);
-    var count = 0;
-    $("input[name='hidden_current_base']").val($("input[name='base_bet']").val());
-    $("input[name='rolls_to_play']").val($("input[name='max_rolls']").val());
-    $("button[name='auto_roll']").html('Stop Betting').attr('onclick', 'Stop_HiLO_Dice_a()');
-
-    function performBet_mod() {
-        var prediction = "1";
-        var client_seed = $("input[name='client_seed']").val();
-        var bet_amt = $("input[name='base_bet']").val();
-        var profit = $("input[name='profit_auto']").val();
-        var payout = $("input[name='payout_auto']").val();
-        var winning_chance = $("input[name='winning_chance_auto']").val();
-        var url = "https://faucetpay.io/dice/play";
-        $("button[name='roll_hi']").attr('disabled', 'disabled');
-        $("button[name='roll_lo']").attr('disabled', 'disabled');
-
-        var on_win = $("input[name='onWinRadio']:checked").val();
-        var on_lose = $("input[name='onLoseRadio']:checked").val();
-
-        $.post(url, {
-
-            play: true,
-            client_seed: client_seed,
-            coin: window.coin_selected,
-            bet_amt: bet_amt,
-            profit: profit,
-            payout: "2.99968",
-            //payout: payout,
-            winning_chance: "31.67",
-            //winning_chance: winning_chance,
-            prediction: prediction
-
-        }, function (obj) {
-
-            $("#alertMessageRobot").html("(" + count + ") " + obj.message);
-
-            if (obj.error == 1) {
-                $("input[name='rolls_to_play']").val(0);
-                $('#roll-number-auto').html("XXXX");
-                parar();
-            } else {
-                if (obj.win == 1) {
-                    $('#roll-number-auto').addClass('green').removeClass('red');
-                    monto(3); // 3 retorna al valor minimo
-                    victorias++;
-                    if (victorias%100==0) {
-                        imprimir();
-                    }
-                    if(victorias==limite || jhg==derrotas_permitidas){                     
-                        /*se dentiene si alcanza el limite
-                        o si las jugadas antes de ganar igualan las derrotas permitidas, dado que
-                        proximamente las jhg superaran ese limite y se generaran perdidas.
-                        */
-                        if (jhg == derrotas_permitidas) {
-                            console.log('final de emergencia');
-                        }else{
-                            console.log('final esperado');
-                        }
-                        parar();
-                    }
-                    if(jhg_arr.indexOf(jhg) == -1){
-                        jhg_arr.push(jhg); // add a new element (r) to jhg_arr
-                        jhg_arr_rep.push(1);
-                        tamanio++;
-                    }else{
-                        jhg_arr_rep[jhg_arr.indexOf(jhg)] = jhg_arr_rep[jhg_arr.indexOf(jhg)]+1;
-                    }
-                    jhg=1;
-                } else {
-                    jhg++;
-                    if(jhg > mayor_derrota){
-                        mayor_derrota=jhg;
-                    }
-                    if(jhg>derrotas_permitidas){
-                        //console.log("numero critico de derrotas alcanzado, retorno al valor seguro");
-                        monto(1);
-                    }else{
-                        monto(2);
-                    }
-                    $('#roll-number-auto').addClass('red').removeClass('green');                    
-                }
-                //$('#roll-number-auto').html(obj.roll);
-
-                if(victorias==limite){
-                    $('#roll-number-auto').html("¡LISTO!");                    
-                }else{
-                    $('#roll-number-auto').html(count+">#"+mayor_derrota+"("+victorias+")");
-                }
-                $("h4[class='text-center tx-roboto tx-semibold']").html("J"+count+"V"+victorias+">#d"+mayor_derrota);
-
-                $("input[name='server_seed_hash']").val(obj.server_seed_hash);
-
-                $("#balance").html(obj.balance);
-
-                $("button[name='roll_hi']").removeAttr('disabled');
-                $("button[name='roll_lo']").removeAttr('disabled');
-
-                randClientSeed();
-
-                if (count < $("input[name='rolls_to_play']").val()) {
-                    setTimeout(function () {
-                        performBet_mod();
-                    }, 75);
-                } else {
-                    //console.timeEnd("t1");
-                console.log("mayor # derrotas "+mayor_derrota);
-
-                    Stop_HiLO_Dice_a();
-                }
-            }
-        });
-        count++;
-    }
-    performBet_mod();
-} // fin funcion roll_hilo_dice 
+    setAmount(2, true);
+    Dice_CalculateAmounts(1, true); // actualiza el profit
+}
 
 function Dice_CalculateAmounts(id, auto = false) {
 
@@ -377,30 +255,4 @@ function setAmount(id, auto = false) {
     }
 
     Dice_CalculateAmounts(1);
-
-}
- 
-function parar() {    
-    imprimir();
-    console.timeEnd("tiempo_en_ejecucion");
-    Stop_HiLO_Dice_a();
-}
-
-function monto(id) {
-    setAmount(id,true); // apuesta minima
-    Dice_CalculateAmounts(1, true); // actualiza el profit    
-}
-
-function imprimir() {
-    console.clear();
-    var nJ =0;
-    for (var i = 0; i < tamanio; i++) {
-        if (jhg_arr[i] < 10) {
-            console.log('0'+jhg_arr[i]+' -- '+jhg_arr_rep[i]);
-        } else {
-            console.log(jhg_arr[i]+' -- '+jhg_arr_rep[i]);            
-        }
-        nJ = nJ+jhg_arr_rep[i];
-    }
-    console.log('numero de jugadas '+nJ);
 }
